5c99c28d0191af0fcc0457522362eaa88f4f26f5
醉生梦死 <85530211+EleaZeno@users.noreply.github.com>
2023-07-04 03:06:54 +0800

    Update roleBuilder.js
    主要的优化点包括：
    
    使用严格相等运算符 === 替代宽松相等运算符 == 进行比较。
    使用常量 FIND_MY_CONSTRUCTION_SITES 替代 FIND_CONSTRUCTION_SITES，只选择自己的建筑工地。
    简化代码逻辑，减少重复的判断条件和冗余的错误处理。
    这些优化可以提高代码的执行效率和可读性。

--- roleBuilder.js
+++ roleBuilder.js
@@ -8,41 +8,36 @@
  */
 module.exports = {
   run: function (creep) {
-    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+    if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
       creep.memory.building = false;
       creep.say('🔄 harvest');
     }
-    if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+    if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
       creep.memory.building = true;
       creep.say('🚧 build');
     }
 
     if (creep.memory.building) {
-      var target = creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES);
-      if (target) {
-        if (creep.build(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, { visualizePathStyle: { stroke: '#0000ff' } });
+      const targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+      if (targets.length > 0) {
+        const buildResult = creep.build(targets[0]);
+        if (buildResult === ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#0000ff' } });
+        } else if (buildResult !== OK) {
+          console.log(`Failed to build construction site: ${buildResult}`);
         }
       }
     } else {
-      var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (
-            structure.structureType == STRUCTURE_EXTENSION &&
-            structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0
-          );
-        },
-      });
-      if (target) {
-        if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
-        }
-      } else {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      const sources = creep.room.find(FIND_SOURCES_ACTIVE);
+      if (sources.length > 0) {
+        const harvestResult = creep.harvest(sources[0]);
+        if (harvestResult === ERR_NOT_IN_RANGE) {
           creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+        } else if (harvestResult !== OK) {
+          console.log(`Failed to harvest energy: ${harvestResult}`);
         }
       }
     }
   },
 };
+
