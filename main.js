8063c3a6d41f72f99cf1de7b796a3b262840c139
醉生梦死 <85530211+EleaZeno@users.noreply.github.com>
2023-07-04 02:21:53 +0800

    重命名
    使用小驼峰法统一命名

--- main.js
+++ main.js
@@ -1,42 +1,49 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require('roleHarvester');
+var roleUpgrader = require('roleUpgrader');
+var roleBuilder = require('roleBuilder');
 var spawner = require('spawner');
 var pathFinder = require('pathFinder');
 var memoryUtils = require('memoryUtils');
 
-var lastMemoryCleanupTick = 0;
-var lastSpawningTextUpdateTick = 0;
-var lastPathFindingTick = 0; // 上次路径计算的 Tick
-var pathFindingInterval = 10; // 路径计算的间隔
-var allCreeps = Object.values(Game.creeps);
-
-
-    module.exports.loop = function () {
-    // 内存清理
+module.exports.loop = function () {
+  // 内存清理
+  var lastMemoryCleanupTick = 0; // 上次内存清理的 Tick
+  if (Game.time - lastMemoryCleanupTick >= 100) {
     memoryUtils.cleanUpMemory();
+    lastMemoryCleanupTick = Game.time;
+  }
 
-    // 获取筛选后的角色数组
-    var harvesters = memoryUtils.getHarvesters();
-    var upgraders = memoryUtils.getUpgraders();
-    var builders = memoryUtils.getBuilders();
+  // 获取筛选后的角色数组
+  var harvesters = memoryUtils.getHarvesters();
+  var upgraders = memoryUtils.getUpgraders();
+  var builders = memoryUtils.getBuilders();
 
-    // 角色执行逻辑
-    for (var name in harvesters) {
-        roleHarvester.run(harvesters[name]);
-    }
+  // 角色执行逻辑
+  for (var name in harvesters) {
+    roleHarvester.run(harvesters[name]);
+  }
 
-    for (var name in upgraders) {
-        roleUpgrader.run(upgraders[name]);
-    }
+  for (var name in upgraders) {
+    roleUpgrader.run(upgraders[name]);
+  }
 
-    for (var name in builders) {
-        roleBuilder.run(builders[name]);
-        
-    }
-    // 生成新的 creep
+  for (var name in builders) {
+    roleBuilder.run(builders[name]);
+  }
+
+  // 生成新的 creep
+  var lastSpawningTextUpdateTick = 0; // 上次生成提示更新的 Tick
+  if (Game.time - lastSpawningTextUpdateTick >= 5) {
     spawner.spawnCreeps();
-    // 路径计算函数
+    lastSpawningTextUpdateTick = Game.time;
+  }
+
+  // 路径计算函数
+  var lastPathFindingTick = 0; // 上次路径计算的 Tick
+  var pathFindingInterval = 10; // 路径计算的间隔
+  if (Game.time - lastPathFindingTick >= pathFindingInterval) {
+    var allCreeps = Object.values(Game.creeps);
     pathFinder.calculatePaths(allCreeps, lastPathFindingTick, pathFindingInterval);
+    lastPathFindingTick = Game.time;
+  }
 };
-    //测试
