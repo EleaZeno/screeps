14bbb1a4890750a341e98e7645b24ced32182798
醉生梦死 <85530211+EleaZeno@users.noreply.github.com>
2023-07-04 03:06:29 +0800

    提升upgrader
    主要的优化点包括：
    
    使用严格相等运算符 === 替代宽松相等运算符 == 进行比较。
    使用常量 FIND_SOURCES_ACTIVE 替代 FIND_SOURCES，只选择激活的资源。
    简化代码逻辑，减少重复的判断条件和冗余的错误处理。
    这些优化可以提高代码的执行效率和可读性。

--- roleBuilder.js
+++ roleBuilder.js
@@ -1,3 +1,11 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('roleBuilder');
+ * mod.thing == 'a thing'; // true
+ */
 module.exports = {
   run: function (creep) {
     if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
--- roleUpgrader.js
+++ roleUpgrader.js
@@ -8,42 +8,38 @@
  */
 
 module.exports = {
-    run: function(creep) {
-        if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
-        }
-
-        if (creep.memory.upgrading) {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-        } else {
-            var extensions = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION) &&
-                        structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-            var sources = creep.room.find(FIND_SOURCES);
-            var target = creep.pos.findClosestByPath(extensions.concat(sources));
-            if (target) {
-                if (target.structureType == STRUCTURE_EXTENSION) {
-                    if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target, { visualizePathStyle: { stroke: '#ffaa00' } });
-                    }
-                } else {
-                    if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target, { visualizePathStyle: { stroke: '#ffaa00' } });
-                    }
-                }
-            }
-        }
+  run: function (creep) {
+    if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.upgrading = false;
+      creep.say('🔄 harvest');
     }
+    if (!creep.memory.upgrading && creep.store.getFreeCapacity() === 0) {
+      creep.memory.upgrading = true;
+      creep.say('⚡ upgrade');
+    }
+
+    if (creep.memory.upgrading) {
+      const controller = creep.room.controller;
+      if (controller) {
+        const upgradeResult = creep.upgradeController(controller);
+        if (upgradeResult === ERR_NOT_IN_RANGE) {
+          creep.moveTo(controller, { visualizePathStyle: { stroke: '#ffffff' } });
+        } else if (upgradeResult !== OK) {
+          console.log(`Failed to upgrade controller: ${upgradeResult}`);
+        }
+      }
+    } else {
+      const sources = creep.room.find(FIND_SOURCES_ACTIVE);
+      if (sources.length > 0) {
+        const harvestResult = creep.harvest(sources[0]);
+        if (harvestResult === ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+        } else if (harvestResult !== OK) {
+          console.log(`Failed to harvest energy: ${harvestResult}`);
+        }
+      }
+    }
+  },
 };
 
 
